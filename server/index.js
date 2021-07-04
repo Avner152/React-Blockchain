8937aa4c1a7261ce86974a16b82cdaa874e87ab7
Avner Levy <AvnerX92@gmail.com>
2021-07-05 00:51:36 +0300

    front is updated and changed

--- server/index.js
+++ server/index.js
@@ -1,26 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose  from 'mongoose';
-import cors from 'cors';
-import blockRoutes from './routes/blocks.js';
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import blockRoutes from "./routes/blocks.js";
 
 //mongo pass: qG6hKLBumnBCO5B7
 
-
 const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
-const CONNECTION_URL = "mongodb+srv://dolevp:qG6hKLBumnBCO5B7@blockchain.orxgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL =
+  "mongodb+srv://dolevp:qG6hKLBumnBCO5B7@blockchain.orxgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log('server started successfully')))
-    .catch(err => console.log(err))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log("server started successfully"))
+  )
+  .catch((err) => console.log(err));
 
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
-
-app.use('/blocks', blockRoutes);
-app.use(express.urlencoded({ extended: false}))
+app.use("/blocks", blockRoutes);
+app.use(express.urlencoded({ extended: false }));
